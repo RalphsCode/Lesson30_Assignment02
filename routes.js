bc9149d25727d711207ee00b0a8fdfe67a24d644
RalphsCode <RalphsCode@gmail.com>
2024-10-04 13:58:42 -0700

    Get and Post routes functional.

--- routes.js
+++ routes.js
@@ -1,16 +1,31 @@
 const express = require("express");    // require express just to get router
 const router = new express.Router();
+const app = express();
+
+app.use(express.json());
 
 let items = require("./fakeDb");
 
 // ITEMS Routes
 //  - prefixed with '/users' by index.js
 
+// Return all items
 router.get('/', function(req, res) {
     return res.json(items);
-})  // END items route
+});  // END Route
 
+
+// Add an item
 router.post('/', function(req, res) {
+    const newItem = { name: req.body.name, price: req.body.price };
+    items.push(newItem);
+    res.json ({added: newItem});
+});  // END Route
+
+// Display an item
+router.get('/:name', function(req, res) {
     
-})
+});  // END Route
+
+
 module.exports = router;
