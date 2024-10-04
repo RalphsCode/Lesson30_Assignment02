a5a1f6050f95e8822e99e7caed53aa2d6db68654
RalphsCode <RalphsCode@gmail.com>
2024-10-04 15:15:30 -0700

    All routes functional.

--- fakeDb.js
+++ fakeDb.js
@@ -1,3 +1,3 @@
-global.items = []
+global.items = [{name : "skittles", price : 2.99}, {name: "M&M's", price : 2.50}]
 
 module.exports = items
--- routes.js
+++ routes.js
@@ -24,8 +24,25 @@ router.post('/', function(req, res) {
 
 // Display an item
 router.get('/:name', function(req, res) {
-    
+    const requested = req.params.name
+    const foundItem = items.find(item => item.name === requested)
+    res.send(foundItem);
 });  // END Route
 
+// Update an existing item
+router.patch('/:name', function(req, res) {
+    const requested = req.params.name
+    const itemIdx = items.findIndex(item => item.name === requested)
+    if (itemIdx !== -1) {
+        const newName = req.body.name || items[itemIdx].name;
+        const newPrice = req.body.price !== undefined ? req.body.price : items[itemIdx].price;
+
+        items[itemIdx] = { "name": newName, "price": newPrice };
+
+        res.json( { updated: items[itemIdx] });
+    } else {
+    res.json( {"Not Found": requested} );
+}
+});  // END Route
 
 module.exports = router;
